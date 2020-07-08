a3f2e172328b9656c5444efa911efde8ad7ed06e
danglass <danglass@MacBook-Air-de-admin.local>
2020-07-08 18:52:01 +0200

    Some plot of data | refacto of class request_data | working good ;)

--- request_data.py
+++ request_data.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 class request_data:
 	
-	def __init__(self, limit):
+	def __init__(self, limit, date):
 		self.limit = limit
 		
 		# API key
@@ -18,11 +18,14 @@ class request_data:
 		self.dict["price"] = "https://min-api.cryptocompare.com/data/v2/histoday"
 
 		#  Timestamp of today 
-		date = datetime.now().timestamp()
-		self.timestamp = int(date)
+		today_date = datetime.now().timestamp()
+		self.timestamp = int(today_date)
 	
-		# Value of 1 day in timestamp
+		# Value of 1 day convert in timestamp
 		self.oneday = 86400
+
+		# date of data extraction (Bitcoin launch date == (1297036800))
+		self.date = date
 
 	def __request_data(self, key):
 		url = self.dict[key]
@@ -35,7 +38,6 @@ class request_data:
 		}
 		result = requests.get(url, params=payload).json()
 		result = pd.DataFrame(result['Data'])
-		result = result['Data']
 		return (result)
 
 	def __concat_df(self, df, stack_df):
@@ -45,23 +47,32 @@ class request_data:
 			stack_df = df
 		return (stack_df)
 
+	def __time_travel(self, key, df, stack_df):
+		if key == "price":
+			self.timestamp = df['Data'][0]["time"] - self.oneday
+			df = df['Data']
+			stack_df = self.__concat_df(df, stack_df)
+		if key == "daily_exchange":
+			self.timestamp = df["time"][0] - self.oneday
+			stack_df = self.__concat_df(df, stack_df)
+		return (stack_df)
+	
 	def data(self, key):
 		stack_df = 0
-		# Bitcoin launch date == (1297036800)
-		while 1297036800 < self.timestamp:
+		while self.date < self.timestamp:
 			df = self.__request_data(key)
-			self.timestamp = df[0]["time"] - self.oneday
-			stack_df = self.__concat_df(df, stack_df)
-		return (list(stack_df))
+			stack_df = self.__time_travel(key, df, stack_df)
+		if key == "price":
+			stack_df = pd.DataFrame(list(stack_df))
+		return (stack_df)
 
-
-request = request_data(2000)
-daily_exchange = request.data("price")
-daily_exchange = pd.DataFrame(daily_exchange)
-
+request = request_data(limit=2000, date=1297036800)
+daily_exchange = request.data("daily_exchange")
+#daily_exchange = request.data("price")
 
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
     print(daily_exchange)
+exit(1)
 
 
 daily_exchange.plot(x ='time', y=['high', 'low'])
